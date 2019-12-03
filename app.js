99beee8ebd4578f91fda96b8b879cd58399fef03
Ziyan Wang <ziyanwang@dhcp-rhodes-3315.redrover.cornell.edu>
2019-12-03 16:18:59 -0500

    app

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express')
 
 const app = express()
-
+const path = require('path');
 app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './public', '/table.html'));
+});
+
+// app.get('/voice', function (req, res) {
+//     res.sendFile('./voice.html');
+// });
+
+app.get('/movement',function(req,res){
+    res.sendFile(path.join(__dirname, './public', '/movement.html'));
+});
+
+app.get('/voice',function(req,res){
+    res.sendFile(path.join(__dirname, './public', '/voice.html'));
+});
+// app.get('/movement',function(req,res){
+//     res.redirect('/?badge_id='+req.query.badge_id+'&dataset_id='+req.query.dataset_id);
+// });
+  
+  
+
 
 app.listen(3000,()=>{
 
