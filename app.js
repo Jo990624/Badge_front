3a1bbcce839fdc13ad091cfbf5c6aeafeb9d3d49
diyapeng <50388704+diyapeng@users.noreply.github.com>
2020-04-17 15:36:31 -0400

    pdy update

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express()
 const path = require('path');
 app.use(express.static('public'))
 
+
+
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './public', '/homepage.html'));
 });
